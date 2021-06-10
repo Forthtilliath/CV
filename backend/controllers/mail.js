0cf0ce49c4a163f3805d4fd8926505714e31a34d
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-06-10 22:20:34 +0200

    Configuration pour l'envoi des mails

---
+++ backend/controllers/mail.js
@@ -0,0 +1,70 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+exports.send = (req, res) => {
+   // const transporter = nodemailer.createTransport({
+   //    service: 'gmail',
+   //    auth: {
+   //       user: process.env.GMAIL_USER,
+   //       pass: process.env.GMAIL_PASS,
+   //    },
+   // });
+   const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+         user: process.env.ETHER_USER,
+         pass: process.env.ETHER_PASS,
+      },
+   });
+   const mailOptions = {
+      from: `${req.body.name} <${req.body.email}>`,
+      to: 'Forth <forth.developer@gmail.com>',
+      subject: 'CV : Message from ' + req.body.name,
+      text: req.body.message,
+   };
+   transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+         console.log(error);
+         res.status(400).json({ errors: error });
+      } else {
+         console.log('Email sent: ' + info.response);
+         res.status(200).json({ result: 'success' });
+      }
+   });
+};
+
+// const nodemailer = require('nodemailer');
+
+// exports.send = (req, res) => {
+//    // const transporter = nodemailer.createTransport({
+//    //    service: 'gmail',
+//    //    auth: {
+//    //       user: process.env.GMAIL_USER,
+//    //       pass: process.env.GMAIL_PASS,
+//    //    },
+//    // });
+//    const transporter = nodemailer.createTransport({
+//       host: 'smtp.ethereal.email',
+//       port: 587,
+//       auth: {
+//          user: process.env.ETHER_USER,
+//          pass: process.env.ETHER_PASS,
+//       },
+//    });
+//    const mailOptions = {
+//       from: `${req.body.name} <${req.body.email}>`,
+//       to: `Forth <${process.env.SEND_TO}>`,
+//       subject: 'CV : Message from ' + req.body.name,
+//       text: `Email : ${req.body.email}\nMessage :\n${req.body.message}`,
+//    };
+//    transporter.sendMail(mailOptions, (error, info) => {
+//       if (error) {
+//          console.log(error);
+//          res.status(400).json({ errors: error });
+//       } else {
+//          console.log('Email sent: ' + info.response);
+//          res.status(200).json({ result: 'success' });
+//       }
+//    });
+// };
--- dev/scripts.js
+++ dev/scripts.js
@@ -14,7 +14,6 @@ window.addEventListener('scroll', function () {
 
 // PROGRESS BAR
 let progressBars = document.querySelectorAll('[data-role="progressbar"');
-console.log(progressBars);
 for (let progressBar of progressBars) {
    let bar = document.createElement('div');
    bar.classList.add('bar');
@@ -84,3 +83,52 @@ $(window).scroll(function () {
 
 // AOS
 AOS.init();
+
+// MAIL
+function after_form_submitted(data) {
+   if (data.result == 'success') {
+      $('form#reused_form').hide();
+      $('#success_message').show();
+      $('#error_message').hide();
+   } else {
+      $('#error_message').append('<ul></ul>');
+
+      jQuery.each(data.errors, function (key, val) {
+         $('#error_message ul').append('<li>' + key + ':' + val + '</li>');
+      });
+      $('#success_message').hide();
+      $('#error_message').show();
+
+      //reverse the response on the button
+      $('button[type="button"]', $form).each(function () {
+         $btn = $(this);
+         label = $btn.prop('orig_label');
+         if (label) {
+            $btn.prop('type', 'submit');
+            $btn.text(label);
+            $btn.prop('orig_label', '');
+         }
+      });
+   } //else
+}
+
+$('#reused_form').submit(function (e) {
+   e.preventDefault();
+
+   $form = $(this);
+   //show some response on the button
+   $('button[type="submit"]', $form).each(function () {
+      $btn = $(this);
+      $btn.prop('type', 'button');
+      $btn.prop('orig_label', $btn.text());
+      $btn.text('Sending ...');
+   });
+
+   $.ajax({
+      type: 'POST',
+      url: 'http://localhost:3000/api/mail/send',
+      data: $form.serialize(),
+      success: after_form_submitted,
+      dataType: 'json',
+   });
+});
