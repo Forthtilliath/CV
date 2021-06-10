e677786bfcc78ad123c32ba64c3cc73290a26439
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-06-10 23:53:52 +0200

    Tuto fini

--- backend/controllers/mail.js
+++ backend/controllers/mail.js
@@ -1,5 +1,4 @@
 const nodemailer = require('nodemailer');
-require('dotenv').config();
 
 exports.send = (req, res) => {
    // const transporter = nodemailer.createTransport({
@@ -19,9 +18,9 @@ exports.send = (req, res) => {
    });
    const mailOptions = {
       from: `${req.body.name} <${req.body.email}>`,
-      to: 'Forth <forth.developer@gmail.com>',
+      to: `Forth <${process.env.SEND_TO}>`,
       subject: 'CV : Message from ' + req.body.name,
-      text: req.body.message,
+      text: `Email : ${req.body.email}\nMessage :\n${req.body.message}`,
    };
    transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
@@ -33,38 +32,3 @@ exports.send = (req, res) => {
       }
    });
 };
-
-// const nodemailer = require('nodemailer');
-
-// exports.send = (req, res) => {
-//    // const transporter = nodemailer.createTransport({
-//    //    service: 'gmail',
-//    //    auth: {
-//    //       user: process.env.GMAIL_USER,
-//    //       pass: process.env.GMAIL_PASS,
-//    //    },
-//    // });
-//    const transporter = nodemailer.createTransport({
-//       host: 'smtp.ethereal.email',
-//       port: 587,
-//       auth: {
-//          user: process.env.ETHER_USER,
-//          pass: process.env.ETHER_PASS,
-//       },
-//    });
-//    const mailOptions = {
-//       from: `${req.body.name} <${req.body.email}>`,
-//       to: `Forth <${process.env.SEND_TO}>`,
-//       subject: 'CV : Message from ' + req.body.name,
-//       text: `Email : ${req.body.email}\nMessage :\n${req.body.message}`,
-//    };
-//    transporter.sendMail(mailOptions, (error, info) => {
-//       if (error) {
-//          console.log(error);
-//          res.status(400).json({ errors: error });
-//       } else {
-//          console.log('Email sent: ' + info.response);
-//          res.status(200).json({ result: 'success' });
-//       }
-//    });
-// };
