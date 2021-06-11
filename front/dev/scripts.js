b9e0f6c2d98a2bb8bc439fb4f200b507cb8b63f1
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-06-11 18:46:17 +0200

    Maj css + aos

--- front/dev/scripts.js
+++ front/dev/scripts.js
@@ -83,6 +83,13 @@ $(window).scroll(function () {
 
 // AOS
 AOS.init();
+document.addEventListener('aos:in', ({ detail }) => {
+   console.log('animated in', detail);
+});
+
+document.addEventListener('aos:out', ({ detail }) => {
+   console.log('animated out', detail);
+});
 
 // MAIL
 function after_form_submitted(data) {
